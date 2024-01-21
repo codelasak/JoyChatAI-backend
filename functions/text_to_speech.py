d4dac6f2723fb429167be8aeef73f175f3390f93
codelasak <eshagh.shahnavazi@istanbulticaret.edu.tr>
2024-01-21 13:39:33 +0300

    Added exicting files

---
+++ functions/text_to_speech.py
@@ -0,0 +1,37 @@
+import requests
+from decouple import config
+
+ELEVEN_LABS_API_KEY = config("ELEVEN_LABS_API_KEY")
+
+# Eleven Labs
+# Convert text to speech
+def convert_text_to_speech(message):
+  body = {
+    "text": message,
+    "model_id": "eleven_multilingual_v2",
+    "voice_settings": {
+        "stability": 0.5,
+        "similarity_boost": 0.5
+    }
+  }
+
+  voice_shaun = "mTSvIrm2hmcnOvb21nW2"
+  voice_rachel = "21m00Tcm4TlvDq8ikWAM"
+  voice_antoni = "ErXwobaYiN019PkySvjV"
+  voice_kid ="jBpfuIE2acCO8z3wKNLl"
+
+  # Construct request headers and url
+  headers = { "xi-api-key": ELEVEN_LABS_API_KEY, "Content-Type": "application/json", "accept": "audio/mpeg" }
+  endpoint = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_kid}"
+
+  try:
+    response = requests.post(endpoint, json=body, headers=headers)
+  except Exception as e:
+     return
+
+  if response.status_code == 200:
+      #with open("output.wav", "wb") as f:
+        #f.write(audio_data)
+      return response.content
+  else:
+    return
